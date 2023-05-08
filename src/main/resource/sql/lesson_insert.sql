8243071164211f9912df77ef1c6c5b7b2837e2a5
sesilK <kimsesilxyz@gmail.com>
2023-05-08 11:05:33 +0900

    예약날짜 컬럼 데이터 타입 변경

--- src/main/resource/sql/lesson_insert.sql
+++ src/main/resource/sql/lesson_insert.sql
@@ -4,3 +4,9 @@ INSERT INTO lesson VALUES ( 3, 'APPRENTICE' ,120000,'Kelly Slater');
 
 select *
 from lesson;
+
+select * from reserve;
+
+-- 예약 테이블에서 예약날짜 컬럼 데이터 타입 변경
+ALTER TABLE reserve
+MODIFY reserve_date varchar2(30);
